057bfd47db1388b2e8e9ec94d0ffcad21e87e4d4
shwetakhai <shwetakhai@cybage.com>
2021-03-08 12:55:47 +0530

    Committed the maven project

---
+++ MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
@@ -0,0 +1,18 @@
+package com.jcg.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+class A{
+	private  String TEST ="";
+	
+}
+}
